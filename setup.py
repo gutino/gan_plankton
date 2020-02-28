76f51103640781237fca2db4563be5834a013310
gutino <gustavotino8@gmail.com>
2020-02-28 01:10:14 -0300

    add template from cookiecutter and an initial notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Computer Science undergrad thesis about using GAN's for image feature extraction for the task of classifying planktons species.',
+    author='Gustavo Tino Ferreira',
+    license='',
+)
